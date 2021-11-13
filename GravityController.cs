8bb7ba5e39161728cff85df6e32b27d40ac21c35
Mehmet Bahadır Maktav <bahadirmaktav314@gmail.com>
2021-11-13 04:46:15 +0300

    Update GravityController.cs

--- GravityController.cs
+++ GravityController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GravityController : MonoBehaviour
 {
+    [Header("Initializations")]
     protected CharacterMovementController characterMovementController;
 
     void Awake()
